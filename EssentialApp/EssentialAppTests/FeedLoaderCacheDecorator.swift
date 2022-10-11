2781b2f6f1b3d1e5b12e2675be50a1f94ff59af3
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-10-11 17:48:56 +0530

    Extract make sut method

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecorator.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecorator.swift
@@ -25,17 +25,22 @@ class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
 
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
-        let loader = FeedLoaderStub(result: .success(feed))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = makeSUT(loaderResult: .success(feed))
         
         expect(sut: sut, toCompleteWith: .success(feed))
     }
     
     func test_load_deliversErrorOnLoaderFailure() {
-        let loader = FeedLoaderStub(result: .failure(anyError()))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = makeSUT(loaderResult: .failure(anyError()))
         
         expect(sut: sut, toCompleteWith: .failure(anyError()))
+    }
+    
+    //MARK: Helpers
+    private func makeSUT(loaderResult: FeedLoader.Result, file: StaticString = #file, line: Int = #line) -> FeedLoaderCacheDecorator {
+        let loader = FeedLoaderStub(result: loaderResult)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        return sut
     }
 
 }
