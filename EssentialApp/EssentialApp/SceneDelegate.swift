3f3e197e00d252c12c4df43ae8b0ae3651529179
Amrit Pandey <amrit.panday@coxautoinc.com>
2022-11-07 14:46:35 +0530

    Displays cached feed online when customer does not has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -22,13 +22,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let remoteUrl = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let httpClient = URLSessionHttpClient(urlSession: URLSession(configuration: .ephemeral))
-        let feedLoader = RemoteFeedLoader(url: remoteUrl, client: httpClient)
-        let imageLoader = RemoteFeedImageDataLoader(httpClient: httpClient)
+        let httpClient = makeRemoteClient()
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteUrl, client: httpClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(httpClient: httpClient)
+        
+        let localStoreUrl = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("FeedStore.sqlite")
+        let localStore = try! CoreDataFeedStore(storeUrl: localStoreUrl, bundle: Bundle(for: CoreDataFeedStore.self))
+        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
         
         window?.rootViewController = FeedUIComposer.feedComposedWith(
-            feedLoader: feedLoader,
-            imageLoader: imageLoader)
+            feedLoader: FeedLoaderWithFallbackComposite(
+                primary: FeedLoaderCacheDecorator(
+                    decoratee: remoteFeedLoader,
+                    cache: localFeedLoader),
+                fallback: localFeedLoader),
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                primary: localImageLoader,
+                fallback: FeedImageDataLoaderCacheDecorator(
+                    decoratee: remoteImageLoader,
+                    cache: localImageLoader)))
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -59,6 +72,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    func makeRemoteClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+        case "offline":
+            return AnyFailingHttpClient()
+        default:
+            return URLSessionHttpClient(urlSession: URLSession(configuration: .ephemeral))
+        }
+    }
+}
 
+private class AnyFailingHttpClient: HTTPClient {
+    private class Task: HTTPClientTask {
+        func cancel() { }
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        completion(.failure(NSError(domain: "offline", code: 0)))
+        return Task()
+    }
 }
 
